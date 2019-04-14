17216fef368102dbb18a6492853624788b921b4d
Yoshihiro Tanaka <contact@cordea.jp>
2019-04-14 14:25:07 +0900

    resolve build error

--- buildSrc/src/main/java/Deps.kt
+++ buildSrc/src/main/java/Deps.kt
@@ -7,13 +7,18 @@ object Deps {
     private val ktor_version = "1.1.3"
     val ktor_client_core = "io.ktor:ktor-client-core:$ktor_version"
     val ktor_client_json = "io.ktor:ktor-client-json:$ktor_version"
+    val ktor_client_core_jvm = "io.ktor:ktor-client-core-jvm:$ktor_version"
+    val ktor_client_json_jvm = "io.ktor:ktor-client-json-jvm:$ktor_version"
 
     private val coroutines_version = "1.2.0"
     val coroutines_core_common = "org.jetbrains.kotlinx:kotlinx-coroutines-core-common:$coroutines_version"
+    val coroutines_core = "org.jetbrains.kotlinx:kotlinx-coroutines-core:$coroutines_version"
 
     private val serialization_version = "0.11.0"
     val serialization_runtime_common =
         "org.jetbrains.kotlinx:kotlinx-serialization-runtime-common:$serialization_version"
+    val serialization_runtime =
+        "org.jetbrains.kotlinx:kotlinx-serialization-runtime:$serialization_version"
 
     private val koin_version = "2.0.0-rc-2"
     val koin_core = "org.koin:koin-core:$koin_version"
