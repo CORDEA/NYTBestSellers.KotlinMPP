183d2cef56a9df06364bfb1d924bf9f97f02d2cd
Yoshihiro Tanaka <contact@cordea.jp>
2019-04-20 16:37:26 +0900

    define additional keys

--- shared/src/commonMain/kotlin/jp/cordea/nytbestsellers/ListNamesResponse.kt
+++ shared/src/commonMain/kotlin/jp/cordea/nytbestsellers/ListNamesResponse.kt
@@ -1,8 +1,12 @@
 package jp.cordea.nytbestsellers
 
+import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
 @Serializable
 data class ListNamesResponse(
+    val status: String,
+    val copyright: String,
+    @SerialName("num_results") val numberOfResults: Int,
     val results: List<ListNameResponse>
 )
