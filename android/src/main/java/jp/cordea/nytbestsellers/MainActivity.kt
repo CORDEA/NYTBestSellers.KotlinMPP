56966c636c896594380adb10ac3897567d4d2254
Yoshihiro Tanaka <contact@cordea.jp>
2019-04-20 19:16:45 +0900

    replace with fragment

--- android/src/main/java/jp/cordea/nytbestsellers/MainActivity.kt
+++ android/src/main/java/jp/cordea/nytbestsellers/MainActivity.kt
@@ -3,47 +3,15 @@ package jp.cordea.nytbestsellers
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
-import android.view.View
-import android.widget.AdapterView
-import android.widget.ArrayAdapter
 import androidx.appcompat.app.AppCompatActivity
-import com.xwray.groupie.GroupAdapter
-import com.xwray.groupie.ViewHolder
+import androidx.navigation.findNavController
 import kotlinx.android.synthetic.main.activity_main.*
-import kotlinx.android.synthetic.main.content_main.*
-import org.koin.android.scope.currentScope
-import org.koin.core.parameter.parametersOf
 
-class MainActivity : AppCompatActivity(), MainContract.View {
-    val presenter = currentScope.inject<MainContract.Presenter> { parametersOf(this@MainActivity) }
-
-    private val adapter = GroupAdapter<ViewHolder>()
-
+class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
-
-        spinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
-            override fun onNothingSelected(parent: AdapterView<*>) {
-            }
-
-            override fun onItemSelected(parent: AdapterView<*>, p1: View, p2: Int, p3: Long) {
-                presenter.value.loadItems(parent.selectedItem.toString())
-            }
-        }
-
-        recyclerView.adapter = adapter
-    }
-
-    override fun onResume() {
-        super.onResume()
-        presenter.value.attach()
-    }
-
-    override fun onPause() {
-        super.onPause()
-        presenter.value.detach()
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
@@ -62,16 +30,5 @@ class MainActivity : AppCompatActivity(), MainContract.View {
         }
     }
 
-    override fun updateSpinnerItems(items: List<String>) {
-        spinner.adapter = ArrayAdapter<String>(
-            this,
-            android.R.layout.simple_spinner_dropdown_item,
-            items
-        )
-    }
-
-    override fun updateItems(items: List<MainListItemModel>) {
-        adapter.clear()
-        adapter.addAll(items.map { MainListItem(it) })
-    }
+    override fun onSupportNavigateUp(): Boolean = findNavController(R.layout.activity_main).navigateUp()
 }
--- android/src/main/java/jp/cordea/nytbestsellers/appModule.kt
+++ android/src/main/java/jp/cordea/nytbestsellers/appModule.kt
@@ -4,7 +4,7 @@ import org.koin.core.qualifier.named
 import org.koin.dsl.module
 
 val appModule = module {
-    scope(named<MainActivity>()) {
+    scope(named<MainFragment>()) {
         bindPresenter()
     }
 }
