7d05aaa14f19a69e6266b4ba5c6085659cea5a6e
Yoshihiro Tanaka <contact@cordea.jp>
2019-04-20 16:40:07 +0900

    fetch data correctly

--- shared/src/commonMain/kotlin/jp/cordea/nytbestsellers/ApiClient.kt
+++ shared/src/commonMain/kotlin/jp/cordea/nytbestsellers/ApiClient.kt
@@ -1,9 +1,11 @@
 package jp.cordea.nytbestsellers
 
 import io.ktor.client.HttpClient
+import io.ktor.client.features.defaultRequest
 import io.ktor.client.features.json.JsonFeature
 import io.ktor.client.features.json.serializer.KotlinxSerializer
 import io.ktor.client.request.HttpRequestBuilder
+import io.ktor.client.request.accept
 import io.ktor.client.request.get
 import io.ktor.client.request.parameter
 import io.ktor.http.ContentType
@@ -12,25 +14,32 @@ import io.ktor.http.takeFrom
 
 internal class ApiClient(private val token: String) {
     companion object {
-        private const val BASE_URL = "https://api.nytimes.com/svc/books/v3/"
-        private const val GET_LISTS_PATH = "lists.json"
-        private const val GET_LIST_NAMES_PATH = "lists/names.json"
+        private const val BASE_URL = "https://api.nytimes.com/"
+        private const val BASE_PATH = "svc/books/v3"
+        private const val GET_LISTS_PATH = "$BASE_PATH/lists.json"
+        private const val GET_LIST_NAMES_PATH = "$BASE_PATH/lists/names.json"
     }
 
     private val client = HttpClient {
         install(JsonFeature) {
-            serializer = KotlinxSerializer()
+            serializer = CustomJsonSerializer(
+                KotlinxSerializer().apply {
+                    setMapper(ListNamesResponse::class, ListNamesResponse.serializer())
+                }
+            )
+        }
+        defaultRequest {
+            accept(ContentType.Application.Json)
+            contentType(ContentType.Application.Json)
         }
     }
 
     suspend fun getLists(): ListsResponse = client.get {
         buildApiUrl(GET_LISTS_PATH)
-        contentType(ContentType.Application.Json)
     }
 
     suspend fun getListNames(): ListNamesResponse = client.get {
         buildApiUrl(GET_LIST_NAMES_PATH)
-        contentType(ContentType.Application.Json)
     }
 
     private fun HttpRequestBuilder.buildApiUrl(path: String) {
